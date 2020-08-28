fedffd69489162b30e931da35679bcec59fceaa8
cheonjiwan <giyan1000@naver.com>
2020-08-28 23:19:37 +0900

    greedy

---
+++ 그리디/3-2.py
@@ -0,0 +1,16 @@
+n,m,k = map(int,input().split())
+
+arr = list(map(int,input().split()))
+
+arr.sort(reverse=True)
+count=0
+result=0
+for i in range(m):
+    if count!=3:
+        result += arr[0]
+        count=count+1
+    else:
+        result += arr[1]
+        count=0
+
+print(result)
