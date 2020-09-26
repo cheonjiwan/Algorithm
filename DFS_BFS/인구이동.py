4405a69526d43f712c0d9a9281cc2cd4d7de75b5
cheonjiwan <giyan1000@naver.com>
2020-09-26 22:53:50 +0900

    DFS_BFS

--- DFS_BFS/인구이동.py
+++ DFS_BFS/인구이동.py
@@ -2,48 +2,48 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+n,l,r = map(int,input().rstrip().split())
+data = []
 dx = [-1,0,1,0]
 dy = [0,-1,0,1]
 
-n,l,r = map(int,input().rstrip().split())
-data=[]
 for _ in range(n):
     data.append(list(map(int,input().rstrip().split())))
 
-def bfs(x,y,index):
+def bfs(x,y,group):
+    check[x][y]=True
+    people = data[x][y]
+    count=1
     united = []
     united.append((x,y))
     q = deque([(x,y)])
-    union[x][y] = index
-    people = data[x][y]
-    count=1
-    
     while q:
         x,y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<n and union[nx][ny]==-1:
-                if l <=abs(data[x][y]-data[nx][ny]) <= r:
-                    union[nx][ny] = index
+            ny = y + dy[i]    
+            if 0<=nx<n and 0<=ny<n and not check[nx][ny]:
+                if l<=abs(data[x][y]-data[nx][ny])<=r:
                     q.append((nx,ny))
+                    check[nx][ny]=True
                     people+=data[nx][ny]
                     count+=1
                     united.append((nx,ny))
-    for i,j in united:
-        data[i][j] = people//count
-
-
+    for country in united:
+        data[country[0]][country[1]] = people//count
+    
 total = 0
 while True:
-    union = [[-1]*n for _ in range(n)]
-    index = 0
+    check = [[False]*n for _ in range(n)]
+    group = 0
+    
     for i in range(n):
         for j in range(n):
-            if union[i][j]==-1:
-                bfs(i,j,index)
-                index+=1
-    if index == n*n:
+            if not check[i][j]:
+                bfs(i,j,group)
+                group+=1
+
+    if group==n*n:
         break
     total+=1
 
