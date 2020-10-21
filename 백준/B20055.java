c058d6894e041d5fc9194e32c9c5ae392b2627c0
cheonjiwan <giyan1000@naver.com>
2020-10-21 20:01:19 +0900

    백준

--- 백준/B20055.java
+++ 백준/B20055.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Temp{
@@ -18,11 +19,20 @@ class Temp{
 	}
 }
 
-class Robot{
+class Robot implements Comparable<Robot>{
 	int idx;
 	
 	public Robot(int idx) {
 		this.idx = idx;
+	}
+	
+	@Override
+	public int compareTo(Robot o) {
+		// TODO Auto-generated method stub
+		if(this.idx > o.idx) return -1;
+		else if(this.idx==o.idx)return 0;
+		else return 1;
+		
 	}
 }
 
@@ -44,51 +54,69 @@ public class Main {
 			belt.add(new Temp(false,Integer.parseInt(st.nextToken())));
 		}
 		
+//		PriorityQueue<Robot> robots = new PriorityQueue<>();
 		Deque<Robot> robots = new ArrayDeque<>();
 		
 		while(true) {
 			// 벨트 회전
+			
 			step+=1;
 			Temp tmp = belt.remove(belt.size()-1);
 			belt.add(0, tmp);
-			
+			int len = robots.size();
+			for(int i=0; i<len; i++) {
+				Robot robot = robots.removeLast();
+				robot.idx+=1;
+				if(robot.idx>=n-1) {
+					belt.get(robot.idx).isExist=false;
+					//robots.remove(robot);
+				}else {
+					robots.addFirst(robot);
+				}
+			}
 			
 			// 로봇 이동
-			//step+=1;
-			for(int i=0; i<robots.size(); i++) {
-				Robot robot = robots.removeFirst();
+			
+			len = robots.size();
+			for(int i=0; i<len; i++) {
+				Robot robot = robots.removeLast();
 				Temp cur = belt.get(robot.idx);
 				Temp next = belt.get(robot.idx+1);
 				if(!next.isExist && next.naegudo>0) {
 					cur.isExist=false;
-					//cur.naegudo-=1;
 					next.isExist=true;
 					next.naegudo-=1;
-					belt.set(robot.idx, cur);
-					belt.set(robot.idx+1, next);
+					robot.idx +=1;
+					if (robot.idx<n-1) {
+						robots.addFirst(robot);
+					}
+					else {
+						belt.get(robot.idx).isExist=false;
+					}
 				}
-				robots.addLast(robot);
+				else {
+					robots.addFirst(robot);
+				}
+				
 			}
 			
 			// 로봇 올리기
-			//step+=1;
 			Temp first = belt.get(0);
-			if(!first.isExist) {
+			if(!first.isExist && first.naegudo>0) {
 				first.isExist=true;
 				first.naegudo-=1;
-				robots.add(new Robot(0));
-				//belt.set(0, first);
+				robots.addFirst(new Robot(0));
 			}
 			
-			//step+=1;
-			for (Temp check : belt) {
-				if(check.naegudo==0) {
+			
+			// 내구도 확인
+			for(int i=0; i<belt.size(); i++) {
+				if(belt.get(i).naegudo==0)
 					count+=1;
-					if(count==k) {
-						System.out.println(step);
-						System.exit(0);
-					}
-				}
+			}
+			if(count>=k) {
+				System.out.println(step);
+				System.exit(0);
 			}
 			count=0;
 		}
