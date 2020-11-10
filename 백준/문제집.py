61e372acc2788431681a7f60d577e43d139ac947
cheonjiwan <giyan1000@naver.com>
2020-11-10 15:05:33 +0900

    문제집

--- 백준/문제집.py
+++ 백준/문제집.py
@@ -1,24 +1,15 @@
 import heapq
-from sys import stdin
-
-input = stdin.readline
-n, m = map(int, input().rstrip().split())
-graph = [[] for _ in range(n + 1)]
-indegree = [0] * (n + 1)
-q = []
-result = []
+input=__import__('sys').stdin.readline
+M=lambda:map(int,input().split())
+n,m = M();graph=[[] for _ in range(n+1)];indegree=[0]*(n+1)
+q=[];result=[]
 for _ in range(m):
-    a, b = map(int, input().rstrip().split())
-    graph[a].append(b)
-    indegree[b] += 1
-for i in range(1, n + 1):
-    if indegree[i] == 0:
-        q.append(i)
+    a,b = M();graph[a].append(b);indegree[b]+=1
+for i in range(1,n+1):
+    if indegree[i]==0:q.append(i)
 while q:
-    now = heapq.heappop(q)
-    result.append(now)
+    now=heapq.heappop(q);result.append(now)
     for i in graph[now]:
-        indegree[i] -= 1
-        if indegree[i] == 0:
-            heapq.heappush(q, i)
+        indegree[i]-=1
+        if indegree[i]==0:heapq.heappush(q,i)
 print(" ".join(str(i) for i in result))
