b97062289d01af8dffdfbf1c533d8a62b1de2fd2
cheonjiwan <giyan1000@naver.com>
2020-12-21 17:21:03 +0900

    전화번호 목록

---
+++ 프로그래머스/Level2/전화번호목록.py
@@ -0,0 +1,8 @@
+def solution(phone_book):
+    for i in range(len(phone_book)):
+        for j in range(len(phone_book)):
+            if i==j: continue
+            else:
+                if phone_book[i]==phone_book[j][:len(phone_book[i])]:
+                    return False
+    return True
