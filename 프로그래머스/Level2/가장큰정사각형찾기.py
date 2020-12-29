cc826d20d524f473ae9702603fe7224a581c3347
cheonjiwan <giyan1000@naver.com>
2020-12-29 14:43:58 +0900

    가장 큰 정사각형 찾기

---
+++ 프로그래머스/Level2/가장큰정사각형찾기.py
@@ -0,0 +1,12 @@
+from copy import deepcopy
+def solution(board):
+    dp=deepcopy(board);result=dp[0][0]
+    for i in range(1,len(board)):
+        for j in range(1,len(board[i])):
+            if dp[i][j]!=0:
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+            result=max(result,dp[i][j])
+    return result**2
+
+board=[[1,0,0],[0,0,0]]
+print(solution(board))
