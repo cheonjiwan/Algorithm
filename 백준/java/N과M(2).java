f8222e5b457b142c88b964b554e536fdd9cd0450
cheonjiwan <giyan1000@naver.com>
2021-03-23 00:39:15 +0900

    N과 M(2)

---
+++ 백준/java/N과M(2).java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static boolean[] visited;
+	static int n,m;
+	static ArrayList<Integer> list;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+      
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[n+1];
+		list = new ArrayList<>();
+		solution(0, list);
+	}
+   
+	private static void solution(int depth,ArrayList<Integer> list) {
+	   if(depth==m){
+		   StringBuilder sb = new StringBuilder();
+		   for(int num : list) {
+			   sb.append(num).append(" ");
+		   }
+		   System.out.println(sb.toString());
+		   return;
+	   }
+	   
+	   for(int i=1; i<=n; i++) {
+		   if(!visited[i]){
+			   visited[i]=true;
+			   if(list.size()==0) {
+				   list.add(i);
+				   solution(depth+1,list);
+				   list.remove(list.size()-1);
+			   }
+			   else if(list.get(list.size()-1)<i) {
+				   list.add(i);
+				   solution(depth+1,list);
+				   list.remove(list.size()-1);
+			   }
+			   visited[i]=false;
+		   }
+	   }
+	}
+
+}
